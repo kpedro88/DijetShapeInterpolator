cb22398bd232db4a0865aa71ed6a7d54ac305f45
Kevin Pedro <kpedro88@gmail.com>
2023-04-12 13:42:15 -0500

    syst variations for CRs

--- makeShapes.sh
+++ makeShapes.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-for REGION in cmsdijet cmsdijet_jesUp cmsdijet_jesDown cmsdijet_jerUp cmsdijet_jerDown cmsdijetCRmiddle cmsdijetCRhigh; do
-	OUTPUTS=()
-	for SHAPE in inputs/input_shapes_svj_*_${REGION}.py; do
-		SUFFIX=$(echo $SHAPE | cut -d'_' -f4-6)
-		ONAME=ResonanceShapes_svj_13TeV_${SUFFIX}_${REGION}.root
-		python getResonanceShapes.py -i $SHAPE -o ${ONAME} -f svj -x ${SUFFIX} --massrange 1500 5100 100 --fineBinning
-		OUTPUTS+=($ONAME)
+for REGION in cmsdijet cmsdijetCRmiddle cmsdijetCRhigh; do
+	for SYST in "" _jesUp _jesDown _jerUp _jerDown; do
+		OUTPUTS=()
+		for SHAPE in inputs/input_shapes_svj_*_${REGION}${SYST}.py; do
+			SUFFIX=$(echo $SHAPE | cut -d'_' -f4-6)
+			ONAME=ResonanceShapes_svj_13TeV_${SUFFIX}_${REGION}${SYST}.root
+			python getResonanceShapes.py -i $SHAPE -o ${ONAME} -f svj -x ${SUFFIX} --massrange 1500 5100 100 --fineBinning
+			OUTPUTS+=($ONAME)
+		done
+		hadd ResonanceShapes_svj_13TeV_${REGION}${SYST}.root ${OUTPUTS[@]}
 	done
-	hadd ResonanceShapes_svj_13TeV_${REGION}.root ${OUTPUTS[@]}
 done
 
